2a49f4ca3114b4c4019a8471aef4a078b14b6dda
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-11-22 22:35:35 +0300

    создаст Post интерфейс

---
+++ readme/libs/shared-types/src/lib/post.interface.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  id: string;
+  title: string;
+  videoUrl: string;
+  preview: string;
+  tags: string[];
+  description: string;
+  quote: string;
+  author: string;
+  photo: string;
+  link: string;
+}
