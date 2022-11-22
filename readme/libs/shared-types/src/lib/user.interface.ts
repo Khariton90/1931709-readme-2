7e1339c9812dd96cce9afefe778dcc5faa3efd02
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-11-22 18:26:40 +0300

    опишет общий интерфейс

---
+++ readme/libs/shared-types/src/lib/user.interface.ts
@@ -0,0 +1,11 @@
+export interface User {
+  _id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  passwordHash: string;
+  avatar: string;
+  dateRegister: Date;
+  subscribers: number;
+  posts: number;
+}
