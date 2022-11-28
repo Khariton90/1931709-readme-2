6638171b5bbb634867039e547c8c910977137881
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-11-28 18:42:22 +0300

    Добавит имплементацию users

--- readme/apps/users/src/app/auth/auth.controller.ts
+++ readme/apps/users/src/app/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  constructor(
+    private readonly authService: AuthService
+  ) {}
+
+  @Post('register')
+  async create(@Body() dto: CreateUserDto) {
+    return this.authService.register(dto);
+  }
+}
