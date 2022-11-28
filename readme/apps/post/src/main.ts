d593d6cfae3319f7ccfe2d04ab25bff3c0a7cf48
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-11-28 21:20:07 +0300

    2.10. Создаём проекты, готовим REST

--- readme/apps/post/src/main.ts
+++ readme/apps/post/src/main.ts
@@ -1,8 +1,5 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+ 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
@@ -12,6 +9,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Post» Service')
+    .setDescription('Post service API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
