b0823a07666c76354125fe8a571e504062181885
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-11-29 19:50:58 +0300

    Users: Добавит ResponseUserDto

--- readme/apps/users/src/app/blog-user/blog-user.controller.ts
+++ readme/apps/users/src/app/blog-user/blog-user.controller.ts
@@ -2,6 +2,9 @@ import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { BlogUserService } from './blog-user.service';
+import { plainToClass } from 'class-transformer';
+import { ResponseUserDto } from './dto/response-user.dto';
+
 
 @ApiTags('user')
 @Controller('user')
@@ -14,17 +17,15 @@ export class BlogUserController {
   @ApiResponse({status: HttpStatus.CREATED, description: 'The new user has been created'})
   @ApiResponse({status: HttpStatus.BAD_REQUEST, description: 'Bad Request, invalid data format'})
   @ApiResponse({status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Such a user already exists'})
-  async create(@Body() dto: CreateUserDto) {
+  async create(@Body() dto: CreateUserDto): Promise<ResponseUserDto> {
     const { _id, email, dateRegister, firstname, lastname } = await this.blogUserService.register(dto);
 
-    const userResponse = {
+    return plainToClass(ResponseUserDto, {
       id: _id,
       email,
       dateRegister,
       firstname,
       lastname
-    }
-
-    return userResponse;
+    });
   }
 }
