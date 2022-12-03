245388d6af15822e02654e72329253effa6a9e82
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-12-03 14:34:27 +0300

    3.9. На связи база данных

---
+++ readme/apps/users/src/app/blog-user/blog-user.model.ts
@@ -0,0 +1,54 @@
+import { Document } from 'mongoose';
+import { User } from '@readme/shared-types';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  collection: 'users'
+})
+export class BlogUserModel extends Document implements User {
+  @Prop({
+    required: true
+  })
+  _id: string
+
+  @Prop({
+    required: true,
+    unique: true
+  })
+  public email: string;
+
+  @Prop({
+    required: true
+  })
+  public firstname: string;
+
+  @Prop({
+    required: true
+  })
+  public lastname: string;
+
+  @Prop({
+    required: true
+  })
+  public passwordHash: string;
+
+  @Prop()
+  public avatar: string;
+
+  @Prop({
+    required: true
+  })
+  public dateRegister: Date;
+
+  @Prop({
+    required: true
+  })
+  public subscribers: number;
+
+  @Prop({
+    required: true
+  })
+  public posts: number;
+}
+
+export const BlogUserSchema = SchemaFactory.createForClass(BlogUserModel);
--- readme/apps/users/src/app/blog-user/blog-user.module.ts
+++ readme/apps/users/src/app/blog-user/blog-user.module.ts
@@ -1,11 +1,17 @@
-import { BlogUserMemoryRepository } from './blog-user-memory.repository';
 import { Module } from '@nestjs/common';
 import { BlogUserController } from './blog-user.controller';
 import { BlogUserService } from './blog-user.service';
+import { BlogUserRepository } from './blog-user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogUserModel, BlogUserSchema } from './blog-user.model';
 
 @Module({
-  imports: [],
-  providers: [BlogUserMemoryRepository, BlogUserService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: BlogUserModel.name, schema: BlogUserSchema }
+    ])
+  ],
+  providers: [BlogUserRepository, BlogUserService],
   exports: [BlogUserService],
   controllers: [BlogUserController],
 })
