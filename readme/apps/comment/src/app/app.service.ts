1ab6e3c321b5170353ca561938cf6a7af0f62e42
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2023-01-20 16:40:28 +0300

    Comment: Исправит ExseptionFilters

--- readme/apps/comment/src/app/app.service.ts
+++ readme/apps/comment/src/app/app.service.ts
@@ -1,7 +1,7 @@
 import { CommentDto } from './dto/comment.dto';
 import { CommentEntity } from './comment.entity';
 import { CommentRepository } from './comment.repository';
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { CommentQuery } from './query/comment.query';
 
 @Injectable()
@@ -29,11 +29,11 @@ export class AppService {
     const existComment = await this.commentRepository.findById(id);
 
     if (!existComment) {
-      throw new BadRequestException('Comment not found');
+      throw new NotFoundException('Comment not found');
     }
 
     if (existComment.userId !== userId) {
-      throw new BadRequestException('The user can only delete his comment');
+      throw new UnauthorizedException('The user can only delete his comment');
     }
 
     await this.commentRepository.delete(id);
